38f6c4ca381ffa2fb28460b82ca0e5c5cb8f0e38
tboussar <thombouss@hotmail.com>
2018-07-11 10:51:04 +0200

    add products api

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,15 @@
 # wsgi.py
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello World! on est en finale!"
+
+
+@app.route('/api/v1/products')
+def get_products():
+    return jsonify(the_products= [
+            { 'id': 1, 'name': 'Skello' },
+            { 'id': 2, 'name': 'Socialive.tv' }
+            ])
